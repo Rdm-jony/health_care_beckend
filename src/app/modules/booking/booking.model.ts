c016533f1a7d99c9673ec93c646c9a6755b307c4
Rdm-jony <jonydascse@gmail.com>
2025-09-02 14:15:01 +0600

    refactor time in booking

--- src/app/modules/booking/booking.model.ts
+++ src/app/modules/booking/booking.model.ts
@@ -4,6 +4,8 @@ import { BOOKING_STATUS, IBooking } from "./booking.interface";
 const bookingSchema = new Schema<IBooking>({
     doctor: { type: Schema.Types.ObjectId, required: true, ref: "Doctor" },
     user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    startTime: { type: String, required: true },
+    endTime: { type: String, required: true },
     payment: { type: Schema.Types.ObjectId, ref: "Payment" },
     status: { type: String, enum: Object.values(BOOKING_STATUS), default: BOOKING_STATUS.PENDING }
 }, {
